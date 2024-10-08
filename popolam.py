602c1313718671c78f4d5dbfe7198463eea4cfc0
MilenaSkrt <135116799+MilenaSkrt@users.noreply.github.com>
2024-10-08 14:24:26 +0500

    Update and rename main.py to popolam.py

--- popolam.py
+++ popolam.py
@@ -1,6 +1,10 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+
 def f(x):
     return x ** 2 - 2 * x - 2 * math.cos(x)  # функция: f(x) = x^2 - 2x - 2cos(x)
+
 def dichotomy(a, b, eps):
     iterations = 0  # Инициализация счётчика итераций
     while (b - a) > eps:
@@ -16,8 +20,9 @@ def dichotomy(a, b, eps):
 
     return (a + b) / 2, iterations  # Возвращаем среднее значение и количество итераций
 
+
 # Установим пределы и точность
-a = 0.5
+a = 0.25
 b = 1.0
 eps = 1e-6
 
@@ -27,5 +32,17 @@ minimum_y = f(minimum_x)
 print(f"Минимум функции достигнут при x = {minimum_x:.6f} и f(x) = {minimum_y:.6f}")
 print(f"Количество итераций: {iterations}")
 
+# Построение графика функции
+x_values = np.linspace(-1,1, 1000)  # диапазон значений x
+y_values = [f(x) for x in x_values]  # вычисляем значения функции
 
-
+plt.plot(x_values, y_values, label='f(x) = x^2 - 2x - 2cos(x)', color='blue')
+plt.scatter(minimum_x, minimum_y, color='red', label='Минимум', zorder=5)
+plt.axhline(0, color='black', linewidth=0.8, linestyle='--')  # ось x
+plt.axvline(0, color='black', linewidth=0.8, linestyle='--')  # ось y
+plt.title('График функции')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.legend()
+plt.grid()
+plt.show()
